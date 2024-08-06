290bc6551880ff315cf648e03183c36af1cde497
Dactrtr <dactrtr@gmail.com>
2024-08-06 20:48:44 +0900

    data list view initial view

--- stepTracker/DashBoardView.swift
+++ stepTracker/DashBoardView.swift
@@ -19,7 +19,7 @@ enum HealthMetricContext : CaseIterable, Identifiable{
   }
 }
 
-struct ContentView: View {
+struct DashBoardView: View {
   @State private var selectedStat: HealthMetricContext = .steps
   var isSteps : Bool {selectedStat == .steps}
   var body: some View {
@@ -82,7 +82,7 @@ struct ContentView: View {
       .padding()
       .navigationTitle("Dashboard")
       .navigationDestination(for: HealthMetricContext.self) { metric in
-        Text(metric.title)
+        HealthDataListView(metric: metric)
       }
     }
     .tint(isSteps ? .pink : .indigo)
@@ -90,5 +90,5 @@ struct ContentView: View {
 }
 
 #Preview {
-  ContentView()
+  DashBoardView()
 }
