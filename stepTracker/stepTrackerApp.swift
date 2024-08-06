55395dac8aa493b3244ba6b80c8a08a3b02abb8e
Dactrtr <dactrtr@gmail.com>
2024-08-06 12:44:57 +0900

    Initial commit

---
+++ stepTracker/stepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  stepTrackerApp.swift
+//  stepTracker
+//
+//  Created by Sebastian Zuniga on 06-08-24.
+//
+
+import SwiftUI
+
+@main
+struct stepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
