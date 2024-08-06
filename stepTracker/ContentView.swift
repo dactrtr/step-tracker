dc911f2905bebb1a8912b738054e35f7fc5a50c0
Dactrtr <dactrtr@gmail.com>
2024-08-06 18:34:51 +0900

    Initial dashboard UI

--- stepTracker/ContentView.swift
+++ stepTracker/ContentView.swift
@@ -8,17 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  var body: some View {
+    NavigationStack{
+      ScrollView{
+        VStack(spacing: 20){
+          VStack {
+            HStack{
+              VStack(alignment:.leading){
+                Label("Stepts", systemImage: "figure.walk")
+                  .font(.title3.bold())
+                  .foregroundColor(.pink)
+                Text("Avg: 18k steps")
+                  .font(.caption)
+                  .foregroundStyle(.secondary)
+              }
+              Spacer()
+              Image(systemName: "chevron.right")
+                .foregroundStyle(.secondary)
+            }
+            .padding(.bottom)
+            RoundedRectangle(cornerRadius: 12)
+              .foregroundStyle(.secondary)
+              .frame(height: 150)
+          }
+          .padding()
+          .background(
+            RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+          VStack(alignment:.leading) {
+            VStack(alignment:.leading){
+              Label("Averages", systemImage: "calendar")
+                .font(.title3.bold())
+                .foregroundColor(.pink)
+              Text("last 28 days")
+                .font(.caption)
+                .foregroundStyle(.secondary)
+            }
+            .padding(.bottom)
+            RoundedRectangle(cornerRadius: 12)
+              .foregroundStyle(.secondary)
+              .frame(height: 240)
+          }
+          .padding()
+          .background(
+            RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
         }
-        .padding()
+      }
+      .padding()
+      .navigationTitle("Dashboard")
     }
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
