661e66ca48700b84602c03f89256132278a6be26
Dactrtr <dactrtr@gmail.com>
2024-10-08 16:47:47 +0900

    Project reorganization

--- stepTracker/Charts/Charts Utilities/ChartHelper.swift
+++ stepTracker/Charts/Charts Utilities/ChartHelper.swift
@@ -1,15 +1,24 @@
 //
-//  ChartMAth.swift
+//  ChartHelper.swift
 //  stepTracker
 //
-//  Created by Sebastian Zuniga on 31-08-24.
+//  Created by Sebastian Zuniga on 24-09-24.
 //
 
 import Foundation
 import Algorithms
 
-struct ChartMath{
+struct ChartHelper{
+  static func  convert(data: [HealthMetric]) -> [DateValueChartData]{
+    data.map { .init(date: $0.date, value: $0.value)}
+  }
   
+  static func parseSelectedData (for data:[DateValueChartData], in selectedDate: Date?) -> DateValueChartData? {
+    guard let  selectedDate else { return nil }
+    return data.first {
+      Calendar.current.isDate(selectedDate, inSameDayAs: $0.date)
+    }
+  }
   static func averageWeekdayCount(for metric: [HealthMetric]) -> [DateValueChartData]{
     let sortedByWeekday = metric.sorted(using: KeyPathComparator(\.date.weekdayInt, order: .forward))
     let weekdayArray = sortedByWeekday.chunked { $0.date.weekdayInt == $1.date.weekdayInt } // creates an array from the array with the same datapoint
@@ -61,4 +70,5 @@ struct ChartMath{
     
     return weekdayChartData
   }
+  
 }
